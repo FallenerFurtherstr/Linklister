1951ad89592f2c184797ef136b3915095170dfaa
劲松 朱 <z5584483@163.com>
2019-06-05 16:29:47 +0800

    v0.1 prove itself of existence

---
+++ ListExp/ListExp/源.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <malloc.h>
+
+#define LIST_INIT_SIZE 100
+#define LISTINCREMENT 10
+typedef struct {
+	int* elem;
+	int length;
+	int listsize;
+} SqList;
+
+int main()
+{
+	SqList test;
+	test.elem = (int)malloc(sizeof(LIST_INIT_SIZE));
+	test.length = 5;
+	test.listsize = LIST_INIT_SIZE;
+	return 0;
+}
+
+void DeleteK(SqList* a, int i, int k)
+{
+	for (int j = 0; i + k + j < a->length; j++)
+	{
+		a->elem[i + j] = a->elem[i + k + j];
+	}
+	a->length -= k;
+}
